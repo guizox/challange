590eb1d40254d1d8b622e282aac128e855bc82df
Vitor Vieira <vitor.vieira@skyone.solutions>
2021-01-27 11:43:48 -0300

    feat: first comimt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import data from "./data.json";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <AppBar position="static">
+        <Toolbar>Skyone Chalange</Toolbar>
+      </AppBar>
+
+      <Grid
+        container
+        justify="center"
+        alignContent="center"
+        style={{ marginTop: 50 }}
+      >
+        <Typography variant="h5">
+          OI, Bem vindo ao desafio da Skyone!
+        </Typography>
+      </Grid>
+
+      <Container style={{ marginTop: 60 }}>
+        <Grid container justify="space-evenly">
+          {data.map((item) => (
+            <Grid item xs={1}>
+              <Typography variant="subtitle1">{item}</Typography>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
